3d50d7bb9727e6925f00baa022eda0bdff3cdaab
tsetsefly <tsetsefly@gmail.com>
2017-02-06 22:45:28 -0800

    finished chapter 00, 01

--- back_end_prep/learn_to_program/README.md
+++ back_end_prep/learn_to_program/README.md
@@ -1,4 +1,7 @@
 # Learn to Program, by Chris Pine
 
-## Chapter 1
+## Chapter 0
 [Link to Chapter 1](https://pine.fm/LearnToProgram/chap_00.html)
+
+## Chapter 1
+[Link to Chapter 2](https://pine.fm/LearnToProgram/chap_01.html)
---
+++ back_end_prep/learn_to_program/chapter_01-numbers/calc.rb
@@ -0,0 +1,29 @@
+puts 1 + 2
+
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -52
+
+puts "hours in a year"
+puts 24 * 365
+
+puts "minutes in a decade"
+puts 60 * 24 * 365 * 10
+
+puts "how many seconds in 31 years"
+puts 60 * 60 * 24 * 365 * 31
+
+puts "how many chocolates will you eat in your life?"
+puts 365 * 75
+
+puts "how many years in 1246 million seconds?"
+puts (((1246000000 / 60) / 60) / 24) / 365
