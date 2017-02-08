5f5b5ce43df2eb1b0fa75d27de27cc88e199d4ba
Daniel Tse <tsetsefly@gmail.com>
2017-02-07 17:52:37 -0800

    working on chater 2: letters

---
+++ back_end_prep/learn_to_program/chapter_02-letters/letters.rb
@@ -0,0 +1,15 @@
+# 'Hello.'
+# 'Ruby rocks.'
+# '5 is my favorite number... what is yours?'
+# 'Snoopy says #%^?&*@! when he stubs his toe.'
+# '     '
+# '' # 'empty string'!
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts 'I like' + 'apple pie.' # string arithmatic
+
+
+
