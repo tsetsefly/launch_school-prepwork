33fbbaf771a674ca63822c74a751a1501535883b
Daniel Tse <tsetsefly@gmail.com>
2017-02-09 14:56:02 -0800

    finishing chapter 03

---
+++ back_end_prep/learn_to_program/chapter_03-variables/variables.rb
@@ -0,0 +1,33 @@
+puts '...you can say that again...'
+puts '...you can say that again...'
+
+myString = '...you can say that again...'
+puts myString
+puts myString
+
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name + '.'
+puts 'Wow!  ' + name + ' is a really long name!'
+
+composer = 'Mozart'
+puts composer + ' was "da bomb", in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+
+var = 'just another ' + 'string'
+puts var
+
+var = 5 * (1+2)
+puts var
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
