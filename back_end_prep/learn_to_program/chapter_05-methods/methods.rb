f7885b355d9a360cdb4b02d6ed2ffe2f172b8a02
Daniel Tse <tsetsefly@gmail.com>
2017-02-14 18:45:32 -0800

    method finishing

--- back_end_prep/learn_to_program/chapter_05-methods/methods.rb
+++ back_end_prep/learn_to_program/chapter_05-methods/methods.rb
@@ -16,10 +16,10 @@ puts var1
 puts var2
 puts var3
 
-puts 'What is your full name?'
-name = gets.chomp
-puts 'Did you know there are ' + name.length.to_s +
-     ' characters in your name, ' + name + '?'
+# puts 'What is your full name?'
+# name = gets.chomp
+# puts 'Did you know there are ' + name.length.to_s +
+#      ' characters in your name, ' + name + '?'
 
 letters = 'aAbBcCdDeE'
 puts letters.upcase
@@ -46,9 +46,9 @@ puts str.ljust(lineWidth/2) + str.rjust(lineWidth/2)
 
 # Angry Boss
 
-puts 'What do you want to ask Angry Boss?'
-str = gets.chomp
-puts 'WHADDAYA MEAN "' + str.upcase + '"?!?  YOU\'RE FIRED!!'
+# puts 'What do you want to ask Angry Boss?'
+# str = gets.chomp
+# puts 'WHADDAYA MEAN "' + str.upcase + '"?!?  YOU\'RE FIRED!!'
 
 puts 5**2
 puts 5**0.5
@@ -58,4 +58,40 @@ puts 365%7
 
 puts((5-2).abs)
 puts((2-5).abs)
+
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(1))
+puts(rand(1))
+puts(rand(1))
+puts(rand(99999999999999999999999999999999999999999999999999999999999))
+puts('The weatherman said there is a '+rand(101).to_s+'% chance of rain,')
+puts('but you can never trust a weatherman.')
+
+puts rand(2.50)
+
+srand 1775
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+srand 1775
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1 + Math.sqrt(5))/2)
 
