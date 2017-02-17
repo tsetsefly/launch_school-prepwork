8a73cde5a4c79139f7305d4deda849e6c33cb3b0
tsetsefly <tsetsefly@gmail.com>
2017-02-16 17:20:51 -0800

    finishing 07

--- back_end_prep/learn_to_program/chapter_07-arrays/arrays.rb
+++ back_end_prep/learn_to_program/chapter_07-arrays/arrays.rb
@@ -42,6 +42,59 @@ puts foods.join(', ')
 puts
 puts foods.join('  :)  ') + '  8)'
 
+# since array doesn't point to anything, nothing is printed
 200.times do
 	puts []
 end
+
+
+favorites = []
+favorites.push 'raindrops on roses'
+favorites.push 'whisky on kittens'
+
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+
+puts favorites.pop
+puts favorites
+puts favorites.length
+
+
+#####
+
+word = ''
+words = []
+
+puts 'Type in words you want in your array (type \'END\' when finished)'
+while word != 'END'
+  word = gets.chomp
+  words.push word.to_s
+end
+
+print 'Removing the \'END\': '
+words.pop
+
+def fake_sort(list)
+	return list if list.size <= 1
+
+	i = 0
+	while i < list.length - 1
+		j = i + 1
+	  while j < list.length - 2
+	  	if list[i] > list[i + j]
+	  		list[i], list[i + j] = list[i + j], list[i]
+	  	end
+	  	j = j + 1
+	  end
+	  i = i + 1
+	end
+
+  list
+end
+
+puts 'Here is your alphabetized list with sort!'
+puts words.sort
+
+puts 'Here is your alphabetized list with fake_sort!'
+puts fake_sort(words)
