b56e415d99b4cb7a2b1ad4635fb53b4f0a98e5df
tsetsefly <tsetsefly@gmail.com>
2017-02-20 17:52:18 -0800

    finishing 09

--- back_end_prep/learn_to_program/chapter_09-classes/classes.rb
+++ back_end_prep/learn_to_program/chapter_09-classes/classes.rb
@@ -70,18 +70,170 @@ puts 5.to_eng
 puts 58.to_eng
 
 
+# class Die
+
+# 	def roll
+# 		1 + rand(6)
+# 	end
+
+# end
+
+# dice = [Die.new, Die.new]
+
+# dice.each do |die|
+# 	puts die.roll
+# end
+
+# instance variables are pre-pended with an '@'
+# instance variables are just an object's variables... will last as long as long as the object does
+
 class Die
 
+	# when an object is created its initialize method is always called (if it's created)
+	def initialize
+		roll
+	end
+
 	def roll
-		1 + rand(6)
+		@numberShowing = 1 + rand(6)
+	end
+
+	def showing
+		@numberShowing
 	end
 
 end
 
-dice = [Die.new, Die.new]
+# die = Die.new
+# die.roll
+# puts die.showing
+# puts die.showing
+# die.roll
+# puts die.showing
+# puts die.showing
 
-dice.each do |die|
-	puts die.roll
+puts Die.new.showing
+
+
+class Dragon
+
+	def initialize name
+    @name = name
+    @asleep = false
+    @stuffInBelly     = 10  # He's full.
+    @stuffInIntestine =  0  # He doesn't need to go.
+
+    puts @name + ' is born.'
+  end
+
+  def feed
+    puts 'You feed ' + @name + '.'
+    @stuffInBelly = 10
+    passageOfTime
+  end
+
+  def walk
+    puts 'You walk ' + @name + '.'
+    @stuffInIntestine = 0
+    passageOfTime
+  end
+
+  def putToBed
+    puts 'You put ' + @name + ' to bed.'
+    @asleep = true
+    3.times do
+      if @asleep
+        passageOfTime
+      end
+      if @asleep
+        puts @name + ' snores, filling the room with smoke.'
+      end
+    end
+    if @asleep
+      @asleep = false
+      puts @name + ' wakes up slowly.'
+    end
+  end
+
+  def toss
+    puts 'You toss ' + @name + ' up into the air.'
+    puts 'He giggles, which singes your eyebrows.'
+    passageOfTime
+  end
+
+  def rock
+    puts 'You rock ' + @name + ' gently.'
+    @asleep = true
+    puts 'He briefly dozes off...'
+    passageOfTime
+    if @asleep
+      @asleep = false
+      puts '...but wakes when you stop.'
+    end
+  end
+
+  private
+
+  # "private" means that the methods defined here are
+  # methods internal to the object.  (You can feed
+  # your dragon, but you can't ask him if he's hungry.)
+
+  def hungry?
+    # Method names can end with "?".
+    # Usually, we only do this if the method
+    # returns true or false, like this:
+    @stuffInBelly <= 2
+  end
+
+  def poopy?
+    @stuffInIntestine >= 8
+  end
+
+  def passageOfTime
+    if @stuffInBelly > 0
+      # Move food from belly to intestine.
+      @stuffInBelly     = @stuffInBelly     - 1
+      @stuffInIntestine = @stuffInIntestine + 1
+    else  # Our dragon is starving!
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts @name + ' is starving!  In desperation, he ate YOU!'
+      exit  # This quits the program.
+    end
+
+    if @stuffInIntestine >= 10
+      @stuffInIntestine = 0
+      puts 'Whoops!  ' + @name + ' had an accident...'
+    end
+
+    if hungry?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts @name + '\'s stomach grumbles...'
+    end
+
+    if poopy?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts @name + ' does the potty dance...'
+    end
+  end
+
 end
 
-
+pet = Dragon.new 'Norbert'
+pet.feed
+pet.toss
+pet.walk
+pet.putToBed
+pet.rock
+pet.putToBed
+pet.putToBed
+pet.putToBed
+pet.putToBed
