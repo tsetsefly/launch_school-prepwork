f5e2937dc3c7faf92ad63c981e127f6dcbf780af
Daniel Tse <tsetsefly@gmail.com>
2017-02-24 23:54:03 -0800

    working on exercises

---
+++ back_end_prep/intro_to_programming/01_the_basics/ex01.rb
@@ -0,0 +1,6 @@
+
+first_name = "d"
+last_name = "t"
+full_name = first_name + " " + last_name
+
+puts full_name
---
+++ back_end_prep/intro_to_programming/01_the_basics/ex02.rb
@@ -0,0 +1,10 @@
+num = 1234
+
+puts "thousands place:"
+puts num % 10
+puts "hundreds place:"
+puts num % 100 / 10
+puts "tens place:"
+puts num % 1000 / 100
+puts "ones place:"
+puts num % 10000 / 1000
