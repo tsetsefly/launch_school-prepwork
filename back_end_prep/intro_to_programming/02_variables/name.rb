5d8e7e674f07ed9a562ee9f678d23b894fd1e7dd
Daniel Tse <tsetsefly@gmail.com>
2017-02-26 01:09:11 -0800

    finishing up 02-variables

---
+++ back_end_prep/intro_to_programming/02_variables/name.rb
@@ -0,0 +1,15 @@
+puts "enter yo first name!"
+
+first = gets.chomp
+
+puts "enter yo last name!"
+
+last = gets.chomp
+
+puts "hello " + first + " " + last + "!"
+
+name = first + " " + last
+
+10.times do
+  puts name
+end
