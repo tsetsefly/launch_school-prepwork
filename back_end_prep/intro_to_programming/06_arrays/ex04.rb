495974e8a5f3a34dd256e899e75ff28b33fd4ec1
Daniel Tse <tsetsefly@gmail.com>
2017-03-05 21:52:03 -0800

    finishing arrays

--- back_end_prep/intro_to_programming/06_arrays/ex04.rb
+++ back_end_prep/intro_to_programming/06_arrays/ex04.rb
@@ -4,8 +4,8 @@ arr = [15, 7, 18, 5, 12, 8, 5, 1]
 a = arr.index(5)
 puts a # returns 3 because 5 is at the index of 3
 
-# a = arr.index[5]
-# puts a
+# a = arr.index[5] # returns "ex04.rb:6:in `<main>': undefined method `[]' for #<Enumerator: [15, 7, 18, 5, 12, 8, 5, 1]:index> (NoMethodError)"
+# puts a 
 
 a = arr[5]
 puts a
---
+++ back_end_prep/intro_to_programming/06_arrays/ex05.rb
@@ -0,0 +1,8 @@
+string = "Welcome to America!"
+a = string[6]
+b = string[11]
+c = string[19]
+
+puts a
+puts b
+puts c
