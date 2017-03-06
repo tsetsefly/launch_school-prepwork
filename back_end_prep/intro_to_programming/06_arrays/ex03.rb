1b3b0234b885951832494df9dfab64fdcf1218fe
Daniel Tse <tsetsefly@gmail.com>
2017-03-05 21:46:33 -0800

    finished ex04

---
+++ back_end_prep/intro_to_programming/06_arrays/ex03.rb
@@ -0,0 +1,5 @@
+arr = [["test", "hello", "world"],["example", "mem"]]
+
+# puts arr
+puts arr[1][0]
+puts arr.last.first
---
+++ back_end_prep/intro_to_programming/06_arrays/ex04.rb
@@ -0,0 +1,11 @@
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+# finds the location of 5 and returns the index value / location
+a = arr.index(5)
+puts a # returns 3 because 5 is at the index of 3
+
+# a = arr.index[5]
+# puts a
+
+a = arr[5]
+puts a
